e7c54d8b91cc86e07ad6bc3825577d29f7e1d09f
Tomoyoshi Ogura <tomoyoshi.ogura@gmail.com>
2015-12-08 01:17:39 +0900

    implement addPrograms method

--- app/domain/model/program/Program.scala
+++ app/domain/model/program/Program.scala
@@ -2,6 +2,7 @@ package domain.model.program
 
 import java.time.LocalDateTime
 
+import domain.model.Entity
 import domain.model.channel.Channel
 
-case class Program(id: ProgramId, channel: Channel, name: String, title: String, episode: Int, startTime: LocalDateTime, endTime: LocalDateTime)
+case class Program(id: ProgramId, channel: Channel, name: String, title: String, episode: Int, startTime: LocalDateTime, endTime: LocalDateTime) extends Entity
--- app/port/adapter/service/WebAPIProgramRepository.scala
+++ app/port/adapter/service/WebAPIProgramRepository.scala
@@ -28,6 +28,24 @@ class WebAPIProgramRepository extends ProgramRepository {
     list
   }
 
-  override def allPrograms(date: LocalDate, channels: Seq[Channel]): Seq[Program] = ???
+  override def allPrograms(date: LocalDate, channels: Seq[Channel]): Seq[Program] = {
+    val url = "http://cal.syoboi.jp/cal_chk.php?start=%s&days=1".format(date)
+    val format = DateTimeFormatter.ofPattern("yyyyMMddHHmmss")
+    val xml = XML.load(new URL(url))
+    val list = (xml \\ "ProgItem").map { e =>
+      Program(
+        id = ProgramId((e \ "@PID").text.toLong),
+        channel = Channel(id = ChannelId((e \ "@ChID").text.toLong), name = (e \ "@ChName").text),
+        name = (e \ "@Title").text,
+        title = (e \ "@SubTitle").text,
+        episode = (e \ "@Count").text.toInt,
+        startTime = LocalDateTime.parse((e \ "@StTime").text, format),
+        endTime = LocalDateTime.parse((e \ "@EdTime").text, format)
+      )
+    }.filter { program =>
+      channels.exists(_ == program.channel)
+    }.sortWith((p1, p2) => p1.startTime.isBefore(p2.startTime))
+    list
+  }
 
 }
