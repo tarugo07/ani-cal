16dfe7589ea216372331d274fe82af1fa41a380e
Tomoyoshi Ogura <tomoyoshi.ogura@gmail.com>
2015-12-12 22:50:30 +0900

    fix program list api

--- app/port/adapter/controllers/Application.scala
+++ app/port/adapter/controllers/Application.scala
@@ -1,5 +1,7 @@
 package port.adapter.controllers
 
+import java.time.LocalDate
+
 import domain.model.channel.{Channel, ChannelId}
 import domain.model.program.{Program, ProgramId}
 import play.api.libs.json._
@@ -38,10 +40,25 @@ class Application extends Controller {
     }
   }
 
-  val repository = new HttpProgramRepository
+  val programRepository = new HttpProgramRepository
+  val channels = Seq(
+    Channel(id = ChannelId(value = 1), name = "NHK総合"),
+    Channel(id = ChannelId(value = 2), name = "NHK Eテレ"),
+    Channel(id = ChannelId(value = 3), name = "フジテレビ"),
+    Channel(id = ChannelId(value = 4), name = "日本テレビ"),
+    Channel(id = ChannelId(value = 5), name = "TBS"),
+    Channel(id = ChannelId(value = 6), name = "テレビ朝日"),
+    Channel(id = ChannelId(value = 7), name = "テレビ東京"),
+    Channel(id = ChannelId(value = 8), name = "tvk"),
+    Channel(id = ChannelId(value = 7), name = "テレビ東京"),
+    Channel(id = ChannelId(value = 8), name = "tvk"),
+    Channel(id = ChannelId(value = 19), name = "TOKYO MX"),
+    Channel(id = ChannelId(value = 20), name = "AT-X"),
+    Channel(id = ChannelId(value = 21), name = "アニマックス")
+  )
 
   def index = Action {
-    val programs = repository.allPrograms()
+    val programs = programRepository.allPrograms(LocalDate.now(), channels)
     Ok(Json.toJson(programs))
   }
 
