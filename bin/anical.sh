7f1fbba0533436120a9075d05d78e5e976309787
Tomoyoshi Ogura <tomoyoshi.ogura@gmail.com>
2015-12-15 22:00:02 +0900

    add anical command

---
+++ bin/anical.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+PROGRAM_API_URL="http://localhost:9000/programs"
+
+which jq > /dev/null 2>&1
+if [ $? -eq 1 ]; then
+    echo "not found jq"
+    exit 1
+fi
+
+curl "${PROGRAM_API_URL}" | jq -r ".[] | \"\(.start_time|split(\"T\")|.[1])〜\(.end_time|split(\"T\")|.[1]) [\(.channel.name)] \(.name) / \(.title)\""
+
+exit 0
