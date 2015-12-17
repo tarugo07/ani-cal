04773b8e72de30ffe4028e6e0f7a7a9786f09953
Tomoyoshi Ogura <tomoyoshi.ogura@gmail.com>
2015-12-17 13:04:19 +0900

    add ChannelGroup model

---
+++ app/domain/model/channel/ChannelGroup.scala
@@ -0,0 +1,5 @@
+package domain.model.channel
+
+import domain.model.Entity
+
+case class ChannelGroup(id: ChannelGroupId, name: String) extends Entity
---
+++ app/domain/model/channel/ChannelGroupId.scala
@@ -0,0 +1,5 @@
+package domain.model.channel
+
+import domain.model.ValueObject
+
+case class ChannelGroupId(value: Long) extends ValueObject
