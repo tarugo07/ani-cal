87ea92d7fecce8f548522c1fcfc2101a1561fadd
Tomoyoshi Ogura <tomoyoshi.ogura@gmail.com>
2015-12-24 20:38:45 +0900

    remove to get program of kanto

--- app/application/ProgramApplicationService.scala
+++ app/application/ProgramApplicationService.scala
@@ -10,12 +10,6 @@ class ProgramApplicationService
  val channelGroupRepository: ChannelGroupRepository,
  val programRepository: ProgramRepository) {
 
-  def getProgramGuideOfKanto(date: LocalDate): Seq[Program] = {
-    val group = channelGroupRepository.channelGroupOfId(ChannelGroupId(value = 1))
-    val channels = channelRepository.allChannelsOfGroup(group)
-    programRepository.allProgramsOfDate(date, channels)
-  }
-
   def getProgramGuide(date: LocalDate, groupId: Long): Seq[Program] = {
     val group = channelGroupRepository.channelGroupOfId(ChannelGroupId(value = groupId))
     val channels = channelRepository.allChannelsOfGroup(group)
