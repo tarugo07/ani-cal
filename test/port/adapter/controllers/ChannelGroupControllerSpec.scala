b43e5b786acf0ed3d6cb8c20ddca9357a7fcdeed
Tomoyoshi Ogura <tomoyoshi.ogura@gmail.com>
2015-12-31 17:15:30 +0900

    add test case of channel group list api

---
+++ test/port/adapter/controllers/ChannelGroupControllerSpec.scala
@@ -0,0 +1,31 @@
+package port.adapter.controllers
+
+import domain.model.channel.{ChannelGroup, ChannelGroupId, ChannelGroupRepository, ChannelId}
+import org.specs2.mock._
+import org.specs2.mutable._
+import play.api.test.Helpers._
+import play.api.test._
+
+import scala.util.Try
+
+class ChannelGroupControllerSpec extends Specification with Mockito {
+
+  "ChannelGroupController" should {
+    "response channel group list json" in new WithApplication() {
+      val channelGroups = Seq(
+        ChannelGroup(id = ChannelGroupId(value = 1), name = "テストチャンネル", channelIds = Seq(ChannelId(value = 1)))
+      )
+      val repository = mock[ChannelGroupRepository]
+      val controller = new ChannelGroupController {
+        override protected[this] def newChannelGroupRepository() = repository
+      }
+
+      repository.allChannelGroup() returns Try(channelGroups)
+
+      val result = controller.list(FakeRequest(GET, "/group"))
+      status(result) must equalTo(OK)
+      contentType(result) must beSome.which(_ == "application/json")
+    }
+  }
+
+}
